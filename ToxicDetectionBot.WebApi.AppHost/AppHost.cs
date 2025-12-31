5db136c4c2945234286ee04cd4be5efaac018a35
Nick Martin <nickmartin1e7e@gmail.com>
2025-12-30 21:59:15 -0500

    hc

--- ToxicDetectionBot.WebApi.AppHost/AppHost.cs
+++ ToxicDetectionBot.WebApi.AppHost/AppHost.cs
@@ -1,6 +1,8 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.ToxicDetectionBot_WebApi>("toxicdetectionbot-webapi")
+    .WithHttpHealthCheck("/swagger")
+    .WithHttpHealthCheck("/hangfire")
     .WithUrl("/swagger/index.html", "Swagger");
 
 builder.Build().Run();
